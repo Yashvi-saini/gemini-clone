6b3f5ea25e15894b8ff33275e92cbe24e7e84db6
yashvi <yashvisaini.ys@gmail.com>
2025-10-03 23:55:59 +0530

    api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {URL} from './constants';
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [query,answerquery]=useState('');
+const [result,setresult]=useState(undefined)
 
+
+ const body={
+  "contents": [{
+    "parts":[{"text":"query"}]
+  }]
+ }
+
+const askquestion=async()=>{
+let response= await fetch(URL,{
+  method:"POST",
+  body:JSON.stringify(body)
+})
+ response = await response.json();
+setresult(response.candidates[0].content.parts[0].text)
+}
   return (
-    <>
-   <h1 className='text-5xl  '>Gemini Clone</h1>
-    </>
+   
+    <div>{/*this is outerwrapper*/}
+      <div>{/*this is main container*/}
+        <div className='container h-180 '>{/*this will be welcome container*/}
+           <div className='text-white'>{result}</div>
+        </div>
+        <div className='bg-zinc-800 w-1/2  text-white m-auto rounded-2xl border-zinc-700 pr-3 p-1 border flex h-18xl'>{/*this will be our querycontainer*/}
+          <input type="text"  value={query} onChange ={(event)=>answerquery(event.target.value)} className='w-full h-full p-3 outline-none' placeholder='Ask gemini' />
+          <button onClick={askquestion}>enter</button>
+        </div>
+        
+      </div>
+    </div>
+   
   )
 }
 
