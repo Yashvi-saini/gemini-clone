2d836a24da01f236f6d32497c9e14ea025be8a86
yashvi <yashvisaini.ys@gmail.com>
2025-10-04 17:49:39 +0530

    interacting chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import { useState,useId } from 'react'
 import './App.css'
 import {URL} from './constants';
 import Answer from './components/answer'
 
 function App() {
  const [query,answerquery]=useState('');
-const [result,setresult]=useState(undefined)
-
+const [result,setresult]=useState([])
+const id=useId()
 
  const body={
   contents: [{
@@ -21,27 +21,36 @@ let response= await fetch(URL,{
   headers:{"Content-Type": "application/json"},
   body:JSON.stringify(body)
 })
- response = await response.json();
 
+ response = await response.json();
  let dataString= response.candidates[0].content.parts[0].text;
  dataString= dataString.split("* ");
  dataString=dataString.map((item)=>item.trim())
- {/*console.log(dataString)*/}
- setresult(dataString)
+ setresult([...result,{type:'q',text:query},{type:'a',text:dataString}]);
 }
+console.log(result)
+
   return (
    
     <div className="flex flex-col h-screen">{/*this is outerwrapper*/}
       <div className="flex-1 overflow-y-auto p-6 flex flex-col items-center">{/*this is main container*/}
         <div className='w-full max-w-2xl space-y-4'>{/*this will be welcome container*/}
            <div className='bg-zinc-900 p-4 rounded-2xl border border-zinc-700 shadow-md text-zinc-300'>
-        <ul>
-            {
-              result && result.map((item,index)=>(
-                <li key={index} className="text-left "><Answer ans ={item} totalresult={result.length} index={index}/></li>
-              ))
-            }
-        </ul>
+              <ul>
+              {
+                result.map((item,index)=>(
+                  <div key={index+Math.random()} className={item.type=='q' ? 'flex justify-end':''}>{
+                  item.type=='q'?
+                   <li key={index+Math.random()} className="text-right p-1 border-5 bg-zinc-700 border-zinc-700 rounded-tl-4xl rounded-br-4xl rounded-bl-4xl  w-fit"><Answer ans ={item.text} totalresult={1} index={index} type={item.type}/></li>
+                   :item.text.map((ansitem,ansindex)=>(
+                    <li key={ansindex+Math.random()} className="text-left "><Answer ans ={ansitem} totalresult={item.length} index={ansindex} type={item.type}/></li>
+                   ))
+                  } 
+                   </div>
+                  ))
+              }
+              </ul>
+
             </div>
         </div>
 
--- src/components/answer.jsx
+++ src/components/answer.jsx
@@ -1,10 +1,8 @@
 import {useEffect,useState} from "react"
-const Answer=({ans,totalresult,index})=>{
+const Answer=({ans,totalresult,index,type})=>{
 
  const [heading,setheading]=useState(false);
  const [output,setoutput]=useState(ans);
- console.log(ans,index);
-
  useEffect(()=>{
  if(checkheading(ans)){
     setheading(true);
@@ -26,7 +24,7 @@ const Answer=({ans,totalresult,index})=>{
      <>
      {
         index==0 && totalresult> 1?<span className="pt-2 text-xl block text-white text-3xl">{output}</span>:        heading?<span className="pt-2 text-lg block text-white text-3xl">{output}</span>:
-        <span className="pl-5 ">{output}</span>
+        <span className={type=='q'?'pl-1':'pl-5'}>{output}</span>
      }
        
      </>
